4bdfc32aec53d683dc3707cba31d00ae3cf571b1
sjas <sjas@spark>
2013-02-27 22:27:08 +0100

    fastcommit

--- asdf.sh
+++ asdf.sh
@@ -2,8 +2,8 @@
 #TODO sort out which features are really needed...
 #delete vimtest first
 time rm -rf /cygdrive/c/dev/vimtest
-#first fix compile flags
-time cp -f Make_cyg.mak ..
+    #first fix compile flags
+    #time cp -f Make_cyg.mak ..
 #this has to be executed in /vim/src
 cd ..
 time ./configure --with-x --enable-gui=yes --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vimtest --enable-multibyte
