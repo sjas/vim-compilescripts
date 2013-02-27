64120a01338a7f4045e123a55cabd084fa510ced
sjas <sjas@spark>
2013-02-27 18:43:12 +0100

    removed gui parameter since this will be triggered in the makefile

--- asdf.sh
+++ asdf.sh
@@ -4,7 +4,8 @@
 cp -f Make_cyg.mak ..
 
 #this has to be executed in /vim/src
+#TODO sort out which features are really needed...
 cd ..
-./configure --with-modified-by SJAS --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim --enable-gui
+./configure --with-modified-by SJAS --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim
 make
 make install
