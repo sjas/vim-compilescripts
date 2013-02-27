6177e569fc9993e4f9eebb9d38fe6178dab9dfb5
sjas <sjas@spark>
2013-02-27 18:55:32 +0100

    put gui param back in again so always a gui will be created no matter what the makefile says

--- asdf.sh
+++ asdf.sh
@@ -6,6 +6,6 @@ cp -f Make_cyg.mak ..
 #this has to be executed in /vim/src
 #TODO sort out which features are really needed...
 cd ..
-./configure --with-modified-by SJAS --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim
+./configure --with-modified-by SJAS --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim --enable-gui
 make
 make install
