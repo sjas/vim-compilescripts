8dbd22cd38d0a6ec64816040f6100d34dfb2e837
sjas <sjas@spark>
2013-02-27 18:39:43 +0100

    fixed some minor stuff in the actual script

--- asdf.sh
+++ asdf.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
+
+#first fix compile flags
+cp -f Make_cyg.mak ..
+
 #this has to be executed in /vim/src
 cd ..
-./configure --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim
+./configure --with-modified-by SJAS --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim --enable-gui
 make
 make install
