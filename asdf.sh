586918c0b1dbee6924d8cfd44fc43a9aadfe047d
sjas <sjas@spark>
2013-02-28 01:37:34 +0100

    added compiled by..

--- asdf.sh
+++ asdf.sh
@@ -6,7 +6,7 @@ time rm -rf /cygdrive/c/dev/vimtest
     #time cp -f Make_cyg.mak ..
 #this has to be executed in /vim/src
 cd ..
-time ./configure --with-x --enable-gui=yes --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vimtest --enable-multibyte
+time ./configure --with-x --enable-gui=yes --enable-pythoninterp --with-features=huge --prefix=/cygdrive/c/dev/vim --enable-multibyte --with-compiledby="sjas <mail@sjas.de>"
 time make -f Make_cyg.mak
 time make install
 #test newly compiled vim for flags...
