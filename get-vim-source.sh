8abb47bbb50c97ac111bed94fd6ad89b6f7b2078
sjas <sjas@spark>
2013-02-28 01:38:09 +0100

    pullscript fuer vim src

--- get-vim-source.sh
+++ get-vim-source.sh
@@ -1,5 +1,9 @@
 #!/bin/zsh
-hg clone https://vim.googlecode.com/hg/ vim
+time hg clone https://vim.googlecode.com/hg/ vim
 cd vim
 hg pull
 hg update
+cd ..
+mv vim-compilescripts vim/src
+cd vim/src
+rm ../../get-vim-source.sh
